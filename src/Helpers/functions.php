602cb6f29fd8314f2bd10af2a40ff14ba75b35db
liuxin <lxyang_21@163.com>
2024-05-28 15:46:20 +0800

    int

--- src/Helpers/functions.php
+++ src/Helpers/functions.php
@@ -20,3 +20,17 @@ function formatBankCardNo($no): string
 {
     return substr($no, 0, 3) . '****' . substr($no, -4);
 }
+
+function str_random(int $length = 16): string
+{
+    $string = '';
+
+    while (($len = strlen($string)) < $length) {
+        $size = $length - $len;
+        $bytes = random_bytes($size);
+
+        $string .= substr(str_replace(['/', '+', '='], '', base64_encode($bytes)), 0, $size);
+    }
+
+    return $string;
+}
